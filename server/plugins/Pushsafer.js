ccb2dcb31879323ce0db634f6ac5a296b7122546
Charlie Guthmann <charlie.guthmann@gmail.com>
2020-11-02 13:02:20 +0100

    Update to Pushsafer plugin

--- server/plugins/Pushsafer.js
+++ server/plugins/Pushsafer.js
@@ -24,18 +24,28 @@ function run(trigger, scope, data, config, callback) {
             if (pConf.sound) {
               pushSound = pConf.sound.value;
             }
+
+            var pushTTL = 10; // default
+            if (pConf.ttl) {
+              pushTTL = pConf.ttl.value;
+            }
+
+            var pushVibration = ""; // default
+            if (pConf.vibration) {
+              pushVibration = pConf.vibration.value;
+            }
             
             var msg = {
                 m: data.message,                            // Message (required)
                 t: data.agency+' - '+data.alias,            // Title (optional)
-                d: 'a',                                     // Device or Device Group id (optional)
+                d: pConf.device.value,                      // Device or Device Group id (required)
                 s: pushSound,                               // Sound (value 0-28) (optional)
-                v: '2',                                     // Vibration (empty or value 1-3) (optional)
+                v: pushVibration,                                     // Vibration (empty or value 1-3) (optional)
                 i: '83',                                    // Icon (value 1-98) (optional)
-                l: '10',                                    // Time to Live (optional)
+                l: pushTTL,                                    // Time to Live (optional)
                 pr: pushPri,                                // Priority (optional: -2, -1, 0, 1, 2)
                 re: '60',                                   // Retry (optional: 60-10800 seconds)
-                ex: '60',                                   // Expire (optional: 60-10800 seconds)
+                ex: '120',                                   // Expire (optional: 60-10800 seconds)
                 a: '0',                                     // Answer
                 onerror: function(err) {
                     logger.main.error('Pushsafer:', err);
